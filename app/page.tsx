212f2fd9bc57a4b9c4c79ea9dd04253cc322ec34
Guilherme <guisarria@gmail.com>
2023-08-29 16:41:19 -0300

    feat: tremor init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from '@/components/mode-toggle'
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Button>Click Me!</Button>
+      <ModeToggle />
     </main>
   )
 }
