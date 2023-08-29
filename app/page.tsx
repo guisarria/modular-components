5ab99ffb6c06526ef30ee04e649b84484d4b0aa5
Guilherme <guisarria@gmail.com>
2023-08-29 17:08:46 -0300

    feat: tremor config

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { ModeToggle } from '@/components/mode-toggle'
+import { BarChartTest } from '@/components/test'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Button>Click Me!</Button>
+      <Button className="">Click Me!</Button>
       <ModeToggle />
+      <BarChartTest />
     </main>
   )
 }
---
+++ components/test.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Card, Title, BarChart, Subtitle } from '@tremor/react'
+
+const chartdata2 = [
+  {
+    name: 'Topic 1',
+    'Group A': 890,
+    'Group B': 338,
+    'Group C': 538,
+    'Group D': 396,
+    'Group E': 138,
+    'Group F': 436,
+  },
+  {
+    name: 'Topic 2',
+    'Group A': 289,
+    'Group B': 233,
+    'Group C': 253,
+    'Group D': 333,
+    'Group E': 133,
+    'Group F': 533,
+  },
+]
+
+const dataFormatter = (number: number) => {
+  return '$ ' + Intl.NumberFormat('us').format(number).toString()
+}
+
+export const BarChartTest = () => (
+  <Card>
+    <Title>Writing Contest: Entries</Title>
+    <BarChart
+      className="mt-6"
+      data={chartdata2}
+      index="name"
+      categories={[
+        'Group A',
+        'Group B',
+        'Group C',
+        'Group D',
+        'Group E',
+        'Group F',
+      ]}
+      colors={['blue', 'teal', 'amber', 'rose', 'indigo', 'emerald']}
+      valueFormatter={dataFormatter}
+      yAxisWidth={48}
+    />
+  </Card>
+)
