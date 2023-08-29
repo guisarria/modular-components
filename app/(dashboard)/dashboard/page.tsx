c4b3bdcf818297bc2ef5ac446b1158128203e2bb
Guilherme <guisarria@gmail.com>
2023-08-29 17:40:25 -0300

    dashboard directory

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from '@/components/mode-toggle'
+import { BarChartTest, Test2 } from '@/components/test'
+import { Button } from '@/components/ui/button'
+
+export default function Dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Button className="">Click Me!</Button>
+      <ModeToggle />
+      <BarChartTest />
+      <Test2 />
+    </main>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import { ModeToggle } from '@/components/mode-toggle'
-import { BarChartTest } from '@/components/test'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Button className="">Click Me!</Button>
-      <ModeToggle />
-      <BarChartTest />
     </main>
   )
 }
